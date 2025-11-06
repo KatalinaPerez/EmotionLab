9965994e58889bef5cea04e54386784921a17838
BastianLazo <bastianlazo0933@gmail.com>
2025-11-06 17:22:58 -0300

    Formulario de la escena Waiting room funcional

--- Assets/Resources/My scripts/PreguntasFormulario.cs
+++ Assets/Resources/My scripts/PreguntasFormulario.cs
@@ -3,24 +3,46 @@ using UnityEngine.UI;
 
 public class PreguntasFormulario : MonoBehaviour
 {
+    [Header("Botones de emociones para esta pregunta")]
     public Button[] optionButtons;
 
-    private int selectedIndex = -1;
+    [Header("Sprite para marcar selección")]
     public Sprite selectedImagePrefab;
 
-    public void SelectOption(int index)
+    private int selectedIndex = -1;
+
+    void Start()
     {
-        selectedIndex = index;
+        foreach (Button btn in optionButtons)
+        {
+            btn.onClick.RemoveAllListeners(); 
+        }
 
         for (int i = 0; i < optionButtons.Length; i++)
         {
-            var selectedImage = optionButtons[i].transform.Find("SelectedImage");
-            if (selectedImage != null)
-            {
-                selectedImage.gameObject.SetActive(i == selectedIndex);
-                Instantiate(selectedImagePrefab, optionButtons[i].transform);
-            }
+            int index = i; 
+            optionButtons[i].onClick.AddListener(() => SelectOption(index));
         }
+    }
 
+    public void SelectOption(int index)
+{
+    for (int i = 0; i < optionButtons.Length; i++)
+    {
+        Transform selectedImage = optionButtons[i].transform.Find("SelectedImage");
+        if (selectedImage != null)
+            selectedImage.gameObject.SetActive(false);
+    }
+
+    selectedIndex = index;
+
+    Transform target = optionButtons[selectedIndex].transform.Find("SelectedImage");
+    if (target != null)
+        target.gameObject.SetActive(true);
+}
+
+    public int GetSelectedIndex()
+    {
+        return selectedIndex;
     }
 }
