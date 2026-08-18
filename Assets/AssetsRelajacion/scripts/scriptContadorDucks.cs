e65501c45718145d2642e8c5266bdc9078cb6389
Kathleen Ampuero <k.ampuerotapia@gmail.com>
2026-08-18 01:35:46 -0400

    feat: Contador de patos

--- Assets/AssetsRelajacion/scripts/scriptContadorDucks.cs
+++ Assets/AssetsRelajacion/scripts/scriptContadorDucks.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class scriptContadorDucks : MonoBehaviour
@@ -9,10 +10,15 @@ public class scriptContadorDucks : MonoBehaviour
     private GameObject gDuck;
     public GameObject giantDuck;
     public GameObject mensajeFinal;
+
+    [Header("UI Contador")]
+    [Tooltip("Texto (TMP) que muestra 'X/8 Patos'. Se actualiza automáticamente.")]
+    public TextMeshProUGUI contadorTexto;
+
     void Start()
     {
         gDuck = GameObject.Find("GiantDuck");
-
+        ActualizarContadorUI();
     }
 
     // Update is called once per frame
@@ -27,6 +33,7 @@ public class scriptContadorDucks : MonoBehaviour
         {
             cDucks += 1;
             print(cDucks);
+            ActualizarContadorUI();
         }
         if (cDucks == 8)
         {
@@ -42,7 +49,17 @@ public class scriptContadorDucks : MonoBehaviour
         {
             cDucks -= 1;
             print(cDucks);
+            ActualizarContadorUI();
         }
+    }
+
+    /// <summary>
+    /// Actualiza el texto del contador con el formato "X/8 Patos".
+    /// </summary>
+    void ActualizarContadorUI()
+    {
+        if (contadorTexto != null)
+            contadorTexto.text = $"{cDucks}/8 Patos";
     }
 
 }
